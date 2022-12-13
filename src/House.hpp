a5a808826256d2ac68661967d80699a9ec140f59
Pavel Potemkin <specialforbtsu@gmail.com>
2022-12-13 09:15:46 +0700

    minor class changes

--- src/House.hpp
+++ src/House.hpp
@@ -1,10 +1,16 @@
 #ifndef HOUSE_HPP
 #define HOUSE_HPP
 
+#include <vector>
 #include "Member.hpp"
 
 class House {
+private:
 
-}
+public:
+    House() {};
+
+    void getAvailableDates() {};
+};
 
 #endif
--- src/Rating.hpp
+++ src/Rating.hpp
@@ -8,6 +8,6 @@ public:
     Rating () {};
 
     bool constraintCheck() {};
-}
+};
 
 #endif
